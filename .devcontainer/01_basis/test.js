b14f8362704cd58259cee314fe3a564991012f45
Priyanshu Gautam <143116137+Priyanshu0717@users.noreply.github.com>
2024-09-21 13:47:34 +0530

    Update test.js

--- .devcontainer/01_basis/test.js
+++ .devcontainer/01_basis/test.js
@@ -1,1 +1,12 @@
-console.log("hello myself priyanshu gautam")
+const accountId = 1445533;
+let accountEmail = "gautampriyanshu@598gmail.com";
+var accountPassword = "3654";
+accountCity = "Jaipur";
+
+accountEmail = "gautamhimanshu@586.com";
+accountPassword = "5351";
+
+let accountState;
+
+console.table([accountId, accountPassword, accountCity, accountEmail, accountState]);
+console.log(accountState);
